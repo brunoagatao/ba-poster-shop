1fca1a28fdabf83d7829406266f227c4bf39dbe2
brunoagatao <brunoagatao@hotmail.com>
2018-10-05 22:14:28 +0100

    Added scroll loading for product list

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,5 @@
 const PRICE = 9.99;
+const LOAD_NUM = 10;
 
 new Vue({
   el: '#app',
@@ -8,6 +9,7 @@ new Vue({
     lastSearch: '',
     newSearch: 'anime',
     price: PRICE,
+    results: [],
     items: [],
     cart: []
   },
@@ -20,9 +22,17 @@ new Vue({
         .get('/search/'.concat(this.newSearch))
         .then(function (res) {
           this.lastSearch = this.newSearch;
-          this.items = res.data;
+          this.results = res.data;
+
+          this.appendItems();
           this.loading = false;
         });
+    },
+    appendItems: function () {
+      if (this.items.length < this.results.length) {
+        const append = this.results.slice(this.items.length, this.items.length + LOAD_NUM);
+        this.items = this.items.concat(append);
+      }
     },
     addItem: function (index) {
       this.total += this.price;
@@ -63,7 +73,19 @@ new Vue({
       return '$'.concat(price.toFixed(2));
     }
   },
+  computed: {
+    noMoreItems: function () {
+      return this.results.length && this.results.length > 0;
+    }
+  },
   mounted: function () {
     this.onSubmit();
+    const vueInstance = this;
+
+    const element = document.getElementById('product-list-bottom');
+    const watcher = scrollMonitor.create(element);
+    watcher.enterViewport(function () {
+      vueInstance.appendItems();
+    });
   },
 });
